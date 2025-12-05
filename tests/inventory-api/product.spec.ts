e5c37073b2ee72ee0b5bc52954e80d58f1947801
phl <philipp.sapronov@gmail.com>
2025-12-05 12:58:49 +0200

    dummy test works

--- tests/inventory-api/product.spec.ts
+++ tests/inventory-api/product.spec.ts
@@ -2,6 +2,8 @@ import { test } from '@fixtures/base.extend';
 import { expect } from '@playwright/test';
 
 test('inventory API works', async ({ api }) => {
+  await api.session.setupUserAndProject();
+
   const { data } = await api.admin.mutation('inventory/ProductCreate', {
     variables: { input: { title: 'Test' } },
   });
